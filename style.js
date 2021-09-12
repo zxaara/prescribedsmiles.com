f85d8e76609754cd1de39634ff419d0d1ffb426f
zxaara <90544031+zxaara@users.noreply.github.com>
2021-09-12 02:17:47 -0400

    Update style.js

--- style.js
+++ style.js
@@ -1,1 +1,15 @@
 
+var cardContainer = $('.card-container');
+var submit = $('.submit-btn');
+var image= $(`.confetti`)
+
+submit.on("click", appendSomeoneJoins);
+
+function appendSomeoneJoins(event) {
+  event.preventDefault();
+  var name = $('.name').val();
+
+  cardContainer.append(`
+    <p class="name-card"> Hooray, ${name} joined the newsletter!🥳🎉</p>
+  `);
+}
